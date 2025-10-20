330d602baf63653786b37dff1489b464fced2ca2
Dayeon <hjkl311@naver.com>
2025-10-20 15:39:24 -0400

    revise ObjectPool

--- Assets/Scripts/Room1/CoordinateSystem/BaseBox.cs
+++ Assets/Scripts/Room1/CoordinateSystem/BaseBox.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public abstract class BaseBox : MonoBehaviour
 {
-    public string BoxName { get; set; }
+    public abstract string BoxName { get; set; }
     public Vector3 InitPos { get; protected set; }
     public Quaternion InitRot { get; protected set; }
     public abstract void ReleaseObjectPool();
